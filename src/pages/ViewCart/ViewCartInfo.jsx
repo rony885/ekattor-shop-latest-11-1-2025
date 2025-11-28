0f67b4a9ad7a8e046f445da61c5d7275ff4ebe8c
Muhammad Rony <ha2ronyahamed@gmail.com>
2025-11-28 23:56:03 +0600

    ekattor shop

--- src/pages/ViewCart/ViewCartInfo.jsx
+++ src/pages/ViewCart/ViewCartInfo.jsx
@@ -1,9 +1,29 @@
 // import React, { useContext } from "react";
 // import { Link } from "react-router-dom";
-// import { CartContext } from "../../context/CartContext.jsx";
+// import { CartContext } from "../../context/CartContext";
 
 // const ViewCartInfo = () => {
-//     const { addToCart } = useContext(CartContext);
+//   const { cartItems, removeFromCart, addToCart } = useContext(CartContext);
+
+//   const incrementQty = (product) => {
+//     addToCart(product);
+//   };
+
+//   const decrementQty = (product) => {
+//     if (product.quantity > 1) {
+//       // Decrease quantity by 1
+//       const updatedCart = cartItems.map((item) =>
+//         item.id === product.id ? { ...item, quantity: item.quantity - 1 } : item
+//       );
+//       localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+//       window.location.reload(); // refresh to update context (simplest way)
+//     }
+//   };
+
+//   const totalPrice = cartItems.reduce(
+//     (acc, item) => acc + item.price * item.quantity,
+//     0
+//   );
 
 //   return (
 //     <>
@@ -102,6 +122,7 @@
 //                             <span className="amount full-price">$11,00</span>
 //                           </li>
 //                         </ul>
+
 //                         <ul className="cart-wrap">
 //                           {/* <!-- cart-info start --> */}
 //                           <li className="item-info">
@@ -161,6 +182,7 @@
 //                             <span className="amount full-price">$21,00</span>
 //                           </li>
 //                         </ul>
+
 //                         <ul className="cart-wrap">
 //                           <li className="item-info">
 //                             <div className="item-img">
@@ -219,6 +241,7 @@
 //                           </li>
 //                         </ul>
 //                       </div>
+
 //                       <div className="cart-buttons">
 //                         <Link to="/product" className="btn-style2">
 //                           Continue shopping
@@ -233,6 +256,7 @@
 //                       <textarea rows="10" name="note"></textarea>
 //                     </div>
 //                   </div>
+
 //                   <div className="cart-info-wrap">
 //                     <div className="cart-calculator cart-info">
 //                       <h6>Shipping info</h6>
@@ -362,10 +386,29 @@
 
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { CartContext } from "../../context/CartContext.jsx";
+import { CartContext } from "../../context/CartContext";
 
 const ViewCartInfo = () => {
-  const { cartItems, removeFromCart } = useContext(CartContext);
+  const { cartItems, removeFromCart, addToCart } = useContext(CartContext);
+
+  const incrementQty = (product) => {
+    addToCart(product);
+  };
+
+  const decrementQty = (product) => {
+    if (product.quantity > 1) {
+      const updatedCart = cartItems.map((item) =>
+        item.id === product.id ? { ...item, quantity: item.quantity - 1 } : item
+      );
+      localStorage.setItem("cartItems", JSON.stringify(updatedCart));
+      window.location.reload();
+    }
+  };
+
+  const totalPrice = cartItems.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
 
   return (
     <>
@@ -391,120 +434,234 @@ const ViewCartInfo = () => {
       </section>
 
       <section className="cart-page section-ptb">
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <div className="cart-page-wrap">
-                <div className="cart-wrap-info">
-                  {/* ---------- Empty Cart ---------- */}
-                  {cartItems.length === 0 && (
-                    <h3 style={{ padding: "30px 0", textAlign: "center" }}>
-                      Your cart is empty 😕
-                    </h3>
-                  )}
+        <form method="post">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div className="cart-page-wrap">
+                  <div className="cart-wrap-info">
+                    <div className="cart-item-wrap">
+                      <div className="cart-title">
+                        <h6>My cart:</h6>
+                        <span className="cart-count">
+                          <span className="cart-counter">3</span>
+                          <span className="cart-item-title">Items</span>
+                        </span>
+                      </div>
 
-                  {/* ---------- Cart Items ---------- */}
-                  {cartItems.length > 0 && (
-                    <div className="item-wrap">
-                      {cartItems.map((item) => (
-                        <ul className="cart-wrap" key={item.id}>
-                          {/* PRODUCT INFO */}
-                          <li className="item-info">
-                            <div className="item-img">
-                              <img
-                                src={item.image}
-                                className="img-fluid"
-                                alt={item.name}
-                              />
-                            </div>
-
-                            <div className="item-text">
-                              <span>{item.name}</span>
-                              <span className="item-option">
-                                <span className="item-price">
-                                  ${item.price}
-                                </span>
-                              </span>
-                            </div>
-                          </li>
-
-                          {/* QUANTITY */}
-                          <li className="item-qty">
-                            <div className="product-quantity-action">
-                              <div className="product-quantity">
-                                <div className="cart-plus-minus">
-                                  <input
-                                    type="text"
-                                    value={item.quantity}
-                                    readOnly
-                                  />
+                      <div className="item-wrap">
+                        {cartItems.length === 0 ? (
+                          <p>Your cart is empty</p>
+                        ) : (
+                          cartItems.map((product) => (
+                            <ul key={product.id} className="cart-wrap">
+                              {/* <!-- cart-info start --> */}
+                              <li className="item-info">
+                                <div className="item-img">
+                                  <Link to="/product">
+                                    <img
+                                      // src="/img/menu/home-pro-banner1.jpg"
+                                      src={product.img1}
+                                      className="img-fluid"
+                                      alt="p-1"
+                                    />
+                                  </Link>
                                 </div>
-                              </div>
-                            </div>
 
-                            {/* REMOVE BUTTON */}
-                            <div className="item-remove">
-                              <span className="remove-wrap">
-                                <button
-                                  onClick={() => removeFromCart(item.id)}
-                                  className="text-danger"
-                                >
-                                  Remove
-                                </button>
-                              </span>
-                            </div>
+                                <div className="item-text">
+                                  <Link to="/product">Bluetooth earbuds</Link>
+                                  <span className="item-option">
+                                    <span className="item-title">Color:</span>
+                                    <span className="item-type">Black</span>
+                                  </span>
+                                  <span className="item-option">
+                                    {/* <span className="item-price">$11,00</span> */}
+                                    <span className="item-price">
+                                      {" "}
+                                      {product.newPrice}
+                                    </span>
+                                  </span>
+                                </div>
+                              </li>
+
+                              {/* <!-- cart-qty start --> */}
+                              <li className="item-qty">
+                                <div className="product-quantity-action">
+                                  <div className="product-quantity">
+                                    <div className="cart-plus-minus">
+                                      <button className="dec qtybutton minus">
+                                        <i className="fa-solid fa-minus"></i>
+                                      </button>
+                                      <input
+                                        type="text"
+                                        name="quantity"
+                                        value="1"
+                                      />
+                                      <button className="inc qtybutton plus">
+                                        <i className="fa-solid fa-plus"></i>
+                                      </button>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div className="item-remove">
+                                  <span className="remove-wrap">
+                                    <Link
+                                      to="#"
+                                      className="text-danger"
+                                      onClick={() => removeFromCart(product.id)}
+                                    >
+                                      Remove
+                                    </Link>
+                                  </span>
+                                </div>
+                              </li>
+
+                              {/* <!-- cart-price start --> */}
+                              <li className="item-price">
+                                <span className="amount full-price">
+                                  $11,00
+                                </span>
+                              </li>
+                            </ul>
+                          ))
+                        )}
+                      </div>
+
+                      <div className="cart-buttons">
+                        <Link to="/product" className="btn-style2">
+                          Continue shopping
+                        </Link>
+                        <Link to="/cart-empty" className="btn-style2">
+                          Clear cart
+                        </Link>
+                      </div>
+                    </div>
+                    <div className="special-notes">
+                      <label>Special instructions for seller</label>
+                      <textarea rows="10" name="note"></textarea>
+                    </div>
+                  </div>
+
+                  <div className="cart-info-wrap">
+                    <div className="cart-calculator cart-info">
+                      <h6>Shipping info</h6>
+                      <div
+                        className="culculate-shipping"
+                        id="shipping-calculator"
+                      >
+                        <ul>
+                          <li className="field">
+                            <label>Country</label>
+                            <select>
+                              <option>India</option>
+                              <option>Afghanistan</option>
+                              <option>Austria </option>
+                              <option>Belgium</option>
+                              <option>Bhutan</option>
+                              <option>Canada</option>
+                              <option>France</option>
+                              <option>Germany</option>
+                              <option>Maldives</option>
+                              <option>Nepal</option>
+                            </select>
                           </li>
-
-                          {/* PRICE */}
-                          <li className="item-price">
-                            <span className="amount full-price">
-                              ${item.price * item.quantity}
-                            </span>
+                          <li className="field">
+                            <label>State</label>
+                            <select>
+                              <option>Gujarat</option>
+                              <option>Andaman and Nicobar Islands</option>
+                              <option>Andhra Pradesh</option>
+                              <option>Bihar</option>
+                              <option>Chandigarh</option>
+                              <option>Delhi</option>
+                              <option>Haryana</option>
+                              <option>Jammu and Kashmir</option>
+                              <option>Karnataka</option>
+                              <option>Ladakh</option>
+                            </select>
+                          </li>
+                          <li className="field cpn-code">
+                            <label>Postal/Zip Codes</label>
+                            <input
+                              type="text"
+                              name="q"
+                              placeholder="Zip/Postal Code"
+                            />
                           </li>
                         </ul>
-                      ))}
+                        <div className="shipping-info">
+                          <Link to="#" className="btn btn-style2">
+                            Calculate
+                          </Link>
+                        </div>
+                      </div>
                     </div>
-                  )}
-
-                  {/* Buttons */}
-                  <div className="cart-buttons">
-                    <Link to="/product" className="btn-style2">
-                      Continue shopping
-                    </Link>
-                  </div>
-                </div>
-
-                {/* Right Side Cart Summary */}
-                {cartItems.length > 0 && (
-                  <div className="cart-info-wrap">
                     <div className="cart-total-wrap cart-info">
                       <div className="cart-total">
                         <div className="total-amount">
                           <h6 className="total-title">Total</h6>
-                          <span className="amount total-price">
-                            $
-                            {cartItems
-                              .reduce(
-                                (sum, item) => sum + item.price * item.quantity,
-                                0
-                              )
-                              .toFixed(2)}
-                          </span>
+                          <span className="amount total-price">$56.00</span>
                         </div>
-
+                        <div className="proceed-to-discount">
+                          <input
+                            type="text"
+                            name="discount"
+                            placeholder="Discount code"
+                          />
+                        </div>
                         <div className="proceed-to-checkout">
                           <Link to="/checkout" className="btn btn-style2">
                             Checkout
                           </Link>
                         </div>
+                        <div className="cart-payment-icon">
+                          <ul className="payment-icon">
+                            <li>
+                              <Link to="/">
+                                <img
+                                  src="/img/payment/pay-1.jpg"
+                                  className="img-fluid"
+                                  alt="pay-1"
+                                />
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/">
+                                <img
+                                  src="/img/payment/pay-2.jpg"
+                                  className="img-fluid"
+                                  alt="pay-2"
+                                />
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/">
+                                <img
+                                  src="/img/payment/pay-3.jpg"
+                                  className="img-fluid"
+                                  alt="pay-3"
+                                />
+                              </Link>
+                            </li>
+                            <li>
+                              <Link to="/">
+                                <img
+                                  src="/img/payment/pay-4.jpg"
+                                  className="img-fluid"
+                                  alt="pay-4"
+                                />
+                              </Link>
+                            </li>
+                          </ul>
+                        </div>
                       </div>
                     </div>
                   </div>
-                )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </form>
       </section>
     </>
   );
